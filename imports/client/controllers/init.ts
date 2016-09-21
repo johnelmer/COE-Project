6eb10e86cb29d8ecbc8638d89b442cc2a8a3f47a
Vince Paul Dela Cruz <sekiryuutei47@gmail.com>
2016-09-21 23:56:20 +0800

    transferred to master

---
+++ imports/client/controllers/init.ts
@@ -0,0 +1,20 @@
+import angular from 'angular'
+import angularMeteor from 'angular-meteor'
+import { init, SetModule } from 'ng2now'
+import angularUiBootstrap from 'angular-ui-bootstrap'
+import ngAnimate from 'angular-animate'
+import ngTouch from 'angular-touch'
+import ngSanitize from 'angular-sanitize'
+import uiRouter from 'angular-ui-router'
+
+init()
+
+const dependencies = [
+  angularMeteor,
+  angularUiBootstrap,
+  ngAnimate,
+  ngTouch,
+  ngSanitize,
+  uiRouter
+]
+SetModule('banner', dependencies)
