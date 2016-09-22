0dfd2d88913bc45290bd4324ec642f2394eba557
Vince Paul Dela Cruz <sekiryuutei47@gmail.com>
2016-09-22 23:08:13 +0800

    replaced ng2now with angular2-now

--- imports/client/controllers/BannerViewComponent.js
+++ imports/client/controllers/BannerViewComponent.js
@@ -2,6 +2,8 @@ import { Component, State, bootstrap } from 'angular2-now'
 import './init.js'
 import '../views/banner-view.html'
 
+import _ from 'underscore'
+
 @Component({
   selector: 'banner',
   templateUrl: 'imports/client/views/banner-view.html',
--- imports/client/controllers/init.js
+++ imports/client/controllers/init.js
@@ -1,12 +1,13 @@
 import angular from 'angular'
 import angularMeteor from 'angular-meteor'
-import { SetModule } from 'ng2now'
+import { init, SetModule } from 'angular2-now'
 import angularUiBootstrap from 'angular-ui-bootstrap'
 import ngAnimate from 'angular-animate'
 import ngTouch from 'angular-touch'
 import ngSanitize from 'angular-sanitize'
 import uiRouter from 'angular-ui-router'
-import uiSelect from 'ui-select'
+
+init()
 
 const dependencies = [
   angularMeteor,
@@ -14,7 +15,6 @@ const dependencies = [
   ngAnimate,
   ngTouch,
   ngSanitize,
-  uiSelect,
   uiRouter
 ]
 SetModule('banner', dependencies)
