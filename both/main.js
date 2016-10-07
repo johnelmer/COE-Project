c97ae0d48d3c9f755be682b36b8147ff0d14dd93
Vince Paul Dela Cruz <sekiryuutei47@gmail.com>
2016-10-08 00:30:20 +0800

    chore(/both, /client, .babelrc, .eslintrc): Imported new Classes to both server and client

--- both/main.js
+++ both/main.js
@@ -1,1 +1,3 @@
 import '/imports/both/models/Student.js'
+import '/imports/both/models/Teacher.js'
+import '/imports/both/models/Secretary.js'
--- client/main.js
+++ client/main.js
@@ -1,4 +1,3 @@
 import '/imports/client/controllers/MainComponent'
 import '/imports/client/controllers/NavBarComponent'
 import '/imports/client/controllers/StudentUpsertComponent'
-import { Meteor } from 'meteor/meteor'
