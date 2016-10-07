a29d8a152e0874f1843d929b57360cef2446b116
Vince Paul Dela Cruz <sekiryuutei47@gmail.com>
2016-10-08 00:20:45 +0800

    refactor(SetupAccount.js): Attach User Collection to target
    Removed Transform because it transforms only the last model to be imported to /both directory

--- imports/both/decorators/SetupAccount.js
+++ imports/both/decorators/SetupAccount.js
@@ -1,9 +1,8 @@
+/* eslint-disable no-param-reassign */
 import { Meteor } from 'meteor/meteor'
 
 function SetupAccount(target) {
-  Meteor.users._transform = function(doc) {
-    return new target(doc)
-  }
+  target.collection = Meteor.users
 }
 
 export default SetupAccount
