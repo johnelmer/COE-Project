d6e4cfb130ca60670526bafc6dea8a79ea0259fd
Vince Paul Dela Cruz <sekiryuutei47@gmail.com>
2016-10-11 20:58:33 +0800

    refactor(User model classes): Changed the user class from abstract to concrete
    Removed inheritance of teacher and secretary from the user class, making the user class the only
    
    class pointing to the 'users' table.

--- imports/both/models/Teacher.js
+++
@@ -1,9 +0,0 @@
-import User from './User'
-import SetupAccount from '../decorators/SetupAccount'
-
-@SetupAccount
-class Teacher extends User {
-
-}
-
-export default Teacher
--- imports/both/models/User.js
+++ imports/both/models/User.js
@@ -1,6 +1,9 @@
 import _ from 'underscore'
 import Model from './Model'
 
+import SetupAccount from '../decorators/SetupAccount'
+
+@SetupAccount
 class User extends Model {
 
 }
