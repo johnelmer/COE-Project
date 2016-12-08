3a2a721d49a5fa54add6d81774f12e84e9422577
Sian Paul Lasaga <sianpaullasaga@gmail.com>
2016-12-08 21:30:14 +0800

    add methods

--- imports/both/models/User.js
+++ imports/both/models/User.js
@@ -8,18 +8,6 @@ import SetupAccount from '../decorators/SetupAccount'
 @SetupAccount
 class User extends Model {
 
-  static login(username, password) {
-    Meteor.loginWithPassword(username, password)
-  }
-
-  static logout() {
-    return Meteor.logout()
-  }
-
-  getRole() {
-    return this.profile.role
-  }
-
   setRole(role) {
     if (role === 'Dean') {
       throw new Error('Not allowed!')
@@ -27,9 +15,26 @@ class User extends Model {
     this.profile.role = role
   }
 
-  changePassword(oldPassword, newPassword, callback) {
-    Accounts.changePassword(oldPassword, newPassword, callback)
+  assignSubject(subject) {
+    const subjectDoc = subject
+    delete subjectDoc.courseIds
+    delete subjectDoc.instructors
+    this.profile.subjectsAssigned.push(subjectDoc)
   }
+
+  removeSubjectAssignment(subjectId) {
+    const subjects = this.profile.subjectsAssigned
+    const index = subjects.findIndex(subject => subject._id === subjectId)
+    if (index !== -1) {
+      subjects.splice(index, 1)
+    }
+  }
+
+  addSubjectAssignment(subject, teacher) {
+    teacher.assignSubject(subject)
+    subject.assignTeacher(teacher)
+  }
+
 }
 
 export default User
