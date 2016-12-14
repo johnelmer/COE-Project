a346002ca5903061b7e48c36da39e5b95596a534
Sian Paul Lasaga <sianpaullasaga@gmail.com>
2016-12-14 09:43:02 +0800

    Add methods for enrolling a student

--- imports/both/models/Course.js
+++ imports/both/models/Course.js
@@ -1,3 +1,5 @@
+import _ from 'underscore'
+
 import SetupCollection from '../decorators/SetupCollection'
 import Schemas from '../Schemas'
 
@@ -7,6 +9,14 @@ import Model from './Model'
 class Course extends Model {
 
   static schema = Schemas.Course
+
+  hasLaboratory() {
+    return this.laboratory instanceof 'object'
+  }
+
+  enrollAStudent(student) {
+    this.students.push(_.pick(student, '_id', 'firstName', 'middleName', 'lastName', 'degree', 'yearLevel'))
+  }
 }
 
 export default Course
--- imports/both/models/Student.js
+++ imports/both/models/Student.js
@@ -1,12 +1,18 @@
 import SetupCollection from '../decorators/SetupCollection'
 
 import Model from './Model'
-import Course from './Course'
 
 @SetupCollection('Students')
 class Student extends Model {
   /* Please add schema for this collection */
   static schema = {}
+
+  enrollACourse(course) {
+    const courseDoc = course
+    delete courseDoc.sessions
+    delete courseDoc.students
+    this.courses.push(courseDoc)
+  }
 }
 
 export default Student
