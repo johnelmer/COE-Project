c15d7d66127bf57ac50ffe4bfd69d5df8b70c6bb
Sian Paul Lasaga <sianpaullasaga@gmail.com>
2017-02-04 20:41:13 +0800

    Commented out generateCourse

--- imports/both/models/Subject.js
+++ imports/both/models/Subject.js
@@ -22,19 +22,22 @@ class Subject extends Model {
   removeTeacher(teacherId) {
     this.removeObjectFromArray('teachersAssigned', '_id', teacherId)
   }
-
+/*
   generateCourse(doc) {
     const course = new Course({
       subject: _.pick(this, '_id', 'name', 'courseNumber', 'credits', 'units'),
       stubcode: doc.stubcode,
       lecture: doc.lecture,
       laboratory: doc.laboratory,
+      sessions: [],
+      students: [],
       semester: doc.semester,
     })
     course.save((err) => {
       if (err) { throw new Error('Failed to generate course') }
     })
   }
+*/
 }
 
 export default Subject
