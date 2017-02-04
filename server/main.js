0e502431d586b8cb78b48ae46edcb51ff314894e
Sian Paul Lasaga <sianpaullasaga@gmail.com>
2017-02-04 22:19:35 +0800

    Added onCreateUser

--- server/main.js
+++ server/main.js
@@ -1,2 +1,22 @@
+import { Accounts } from 'meteor/accounts-base'
+
 import '/imports/server/permissions.js'
 import '/imports/server/publications.js'
+
+import User from '/imports/both/models/User'
+
+Accounts.onCreateUser((options, user) => {
+  user.profile = {}
+  const roleName = options.profile.roleName
+  const profileOptions = options.profile
+  if (roleName !== 'Dean') {
+    Object.keys(profileOptions).forEach((key) => {
+      user[key] = profileOptions[key]
+    })
+  }
+  else if (roleName === 'Dean' && User.find({ roleName: 'Dean' }).count() === 1) {
+    user = {}
+    throw new Error('Only 1 dean account allowed!')
+  }
+  return user
+})
