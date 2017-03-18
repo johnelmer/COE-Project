f57dbd3d655b6b325657fdf0f752492aa6be9990
Sian Paul Lasaga <sianpaullasaga@gmail.com>
2017-03-18 22:58:07 +0800

    Added activity

--- imports/server/permissions.js
+++ imports/server/permissions.js
@@ -4,6 +4,7 @@ import Student from '/imports/both/models/Student'
 import Subject from '/imports/both/models/Subject'
 import Degree from '/imports/both/models/Degree'
 import Role from '/imports/both/models/Role'
+import Activity from '/imports/both/models/Activity'
 
 Course.collection.allow({
   insert: () => true,
@@ -30,6 +31,12 @@ Degree.collection.allow({
 })
 
 Role.collection.allow({
+  insert: () => true,
+  update: () => true,
+  remove: () => true,
+})
+
+Activity.collection.allow({
   insert: () => true,
   update: () => true,
   remove: () => true,
--- imports/server/publications.js
+++ imports/server/publications.js
@@ -5,6 +5,7 @@ import Subject from '/imports/both/models/Subject'
 import Degree from '/imports/both/models/Degree'
 import User from '/imports/both/models/User'
 import Role from '/imports/both/models/Role'
+import Activity from '/imports/both/models/Activity'
 
 Meteor.publish('courses', () => Course.find())
 
@@ -17,3 +18,5 @@ Meteor.publish('degrees', () => Degree.find())
 Meteor.publish('teachers', () => User.find({ roleName: 'teacher' }))
 
 Meteor.publish('roles', () => Role.find())
+
+Meteor.publish('activities', () => Activity.find())
