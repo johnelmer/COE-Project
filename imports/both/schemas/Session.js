7b38a965a05911131d57e45b817664cf8c86bc62
Sian Paul Lasaga <sianpaullasaga@gmail.com>
2017-03-21 12:56:06 +0800

    Added attendance component

--- imports/both/schemas/Session.js
+++ imports/both/schemas/Session.js
@@ -1,27 +1,26 @@
 import { SimpleSchema } from 'meteor/aldeed:simple-schema'
 
+const attendance = new SimpleSchema({
+  studentId: {
+    type: String,
+  },
+  type: {
+    type: String,
+    allowedValues: [
+      'Present',
+      'Late',
+      'Absent',
+      'Excuse',
+    ],
+  },
+})
+
 export default new SimpleSchema({
   courseId: {
     type: String,
   },
-  attendance: {
-    type: Object,
-    optional: true,
-  },
-  'attendance.presents': {
-    type: [String],
-    optional: true,
-  },
-  'attendance.lates': {
-    type: [String],
-    optional: true,
-  },
-  'attendance.absents': {
-    type: [String],
-    optional: true,
-  },
-  'attendance.excuses': {
-    type: [String],
+  studentAttendances: {
+    type: [attendance],
     optional: true,
   },
   activityIds: {
