d3e73052b6664465929ab46265f3aa136b4814fc
Sian Paul Lasaga <sianpaullasaga@gmail.com>
2017-04-26 23:25:29 +0800

    Updated: publications

--- imports/server/publications.js
+++ imports/server/publications.js
@@ -6,16 +6,14 @@ import Degree from '/imports/both/models/Degree'
 import User from '/imports/both/models/User'
 import Role from '/imports/both/models/Role'
 import Activity from '/imports/both/models/Activity'
+import ActivityType from '/imports/both/models/ActivityType'
 import Session from '/imports/both/models/Session'
 
 Meteor.publish('courses', () => Course.find())
 
 Meteor.publish('students', () => Student.find({}, { sort: { lastName: 1 } }))
 
-Meteor.publish('students-basic-infos', () => {
-  return Student.find({}, { sort: { lastName: 1 },
-    fields: { firstName: 1, middleName: 1, lastName: 1 } })
-})
+Meteor.publish('students-basic-infos', () => Student.find({}, { fields: { firstName: 1, middleName: 1, lastName: 1 } }))
 
 Meteor.publish('subjects', () => Subject.find())
 
@@ -26,5 +24,7 @@ Meteor.publish('teachers', () => User.find({ roleName: 'teacher' }))
 Meteor.publish('roles', () => Role.find())
 
 Meteor.publish('activities', () => Activity.find())
+
+Meteor.publish('activity-types', () => ActivityType.find({}, { fields: { name: 1 } }))
 
 Meteor.publish('sessions', () => Session.find())
