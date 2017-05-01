8acee4c845cac7482a1a5997b2488fac97a18a19
johnelmer <johnelmer_loretizo@yahoo.com>
2017-05-01 21:29:25 +0800

    rollback changes

---
+++ imports/client/controllers/DepartmentUpsertComponent.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-alert */
+import Department from '/imports/both/models/Department'
+import { Component, State, Inject } from 'angular2-now'
+import '../views/department-upsert.html'
+
+@State({
+  name: 'app.department.create',
+  url: '/department/create',
+})
+@Component({
+  selector: 'dept-upsert',
+  templateUrl: 'imports/client/views/department-upsert.html',
+})
+@Inject('$scope', '$reactive', '$state', '$stateParams')
+class DepartmentUpsertComponent {
+  constructor($scope, $reactive, $state, $stateParams) {
+    $reactive(this).attach($scope)
+    const { departmentId } = $stateParams
+    this.subscribe('departments')
+    this.helpers({
+      department() {
+        if ($state.current.name.endsWith('create')) {
+          return new Department
+        }
+        return Department.findOne({ _id: departmentId })
+      },
+      departments() {
+        return Department.find().fetch()
+      }
+    })
+  }
+
+  save() {
+    this.department.save(() => {
+      alert('Added!')
+    })
+  }
+}
+export default DepartmentUpsertComponent
