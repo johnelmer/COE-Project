eb8720cd4bf10dc727e5ba031ba0e5f7944dc169
johnelmer <johnelmer_loretizo@yahoo.com>
2017-05-01 21:58:32 +0800

    Fixed unexpected token error

--- both/main.js
+++ both/main.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-import '/imports/both/models/Student.js'
-import '/imports/both/models/User.js'
-import '/imports/both/models/Subject.js'
-import '/imports/both/models/Course.js'
-=======
+
 import '/imports/both/models/Student.js'
 import '/imports/both/models/User.js'
 import '/imports/both/models/Subject.js'
 import '/imports/both/models/Course.js'
 import '/imports/both/models/Role.js'
 import '/imports/both/models/Activity.js'
->>>>>>> backend-branch
--- client/main.js
+++ client/main.js
@@ -1,9 +1,6 @@
 import '/imports/client/controllers/MainComponent'
 import '/imports/client/controllers/NavBarComponent'
-<<<<<<< HEAD
 import '../node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js'
-=======
->>>>>>> backend-branch
 import '/imports/client/controllers/StudentUpsertComponent'
 import '/imports/client/controllers/LoginComponent'
 import '/imports/client/controllers/FooterComponent'
@@ -17,14 +14,6 @@ import '/imports/client/controllers/CourseStudentEnrollComponent'
 import '/imports/client/controllers/DegreeUpsertComponent'
 import '/imports/client/controllers/DepartmentUpsertComponent'
 import '/imports/client/controllers/ClassRecordComponent'
-<<<<<<< HEAD
-
-=======
 import '/imports/client/controllers/SubjectAssignmentComponent'
 import '/imports/client/controllers/ActivityUpdateComponent'
 import '/imports/client/controllers/AttendanceUpdateComponent'
-<<<<<<< HEAD
-import '/imports/client/controllers/ClassRecordComponent'
-=======
->>>>>>> backend-branch
->>>>>>> frontend-branch
