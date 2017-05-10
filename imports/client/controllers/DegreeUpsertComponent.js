c50be3f783792a79ffde74d349a2ae17aaa91e28
Vince Paul Dela Cruz <sekiryuutei47@gmail.com>
2017-05-10 14:35:34 +0800

    fix(Stashed Changes):

--- imports/client/controllers/DegreeUpsertComponent.js
+++ imports/client/controllers/DegreeUpsertComponent.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 /* eslint-disable no-alert */
 import Degree from '/imports/both/models/Degree'
 import { Component, State, Inject } from 'angular2-now'
@@ -34,41 +33,3 @@ class DegreeUpsertComponent {
   }
 }
 export default DegreeUpsertComponent
-=======
-/* eslint-disable no-alert */
-import Degree from '/imports/both/models/Degree'
-import { Component, State, Inject } from 'angular2-now'
-import '../views/degree-upsert.html'
-
-@State({
-  name: 'app.degree.create',
-  url: '/degree/create',
-})
-@Component({
-  selector: 'degree-upsert',
-  templateUrl: 'imports/client/views/degree-upsert.html',
-})
-@Inject('$scope', '$reactive', '$state', '$stateParams')
-class DegreeUpsertComponent {
-  constructor($scope, $reactive, $state, $stateParams) {
-    $reactive(this).attach($scope)
-    const { degreeId } = $stateParams
-    this.subscribe('degrees')
-    this.helpers({
-      degree() {
-        if ($state.current.name.endsWith('create')) {
-          return new Degree
-        }
-        return Degree.findOne({ _id: degreeId })
-      },
-    })
-  }
-
-  save() {
-    this.degree.save(() => {
-      alert('Added!')
-    })
-  }
-}
-export default DegreeUpsertComponent
->>>>>>> Stashed changes
