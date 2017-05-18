0fec185b6fb7b23a8eab820cf637f6cb71eddc26
johnelmer <johnelmer_loretizo@yahoo.com>
2017-05-18 13:06:20 +0800

    fix(main js files): Fixed merge conflicts in main js files

--- client/main.js
+++ client/main.js
@@ -1,9 +1,5 @@
 import '/imports/client/controllers/MainComponent'
 import '/imports/client/controllers/NavBarComponent'
-<<<<<<< HEAD
-import '../node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js'
-=======
->>>>>>> backend-branch
 import '/imports/client/controllers/StudentUpsertComponent'
 import '/imports/client/controllers/LoginComponent'
 import '/imports/client/controllers/FooterComponent'
@@ -16,10 +12,6 @@ import '/imports/client/controllers/TeacherViewComponent'
 import '/imports/client/controllers/CourseStudentEnrollComponent'
 import '/imports/client/controllers/DegreeUpsertComponent'
 import '/imports/client/controllers/ClassRecordComponent'
-<<<<<<< HEAD
-
-=======
 import '/imports/client/controllers/SubjectAssignmentComponent'
 import '/imports/client/controllers/ActivityUpdateComponent'
 import '/imports/client/controllers/AttendanceUpdateComponent'
->>>>>>> backend-branch
