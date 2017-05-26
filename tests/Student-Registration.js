ca4f118c2960ced9a1ba1d5cfda496f218c93724
johnelmer <johnelmer_loretizo@yahoo.com>
2017-05-26 13:11:13 +0800

    test(Acceptance Tests): Add acceptance test cases for logging in

---
+++ tests/Student-Registration.js
@@ -0,0 +1,22 @@
+/* eslint-env mocha */
+import { expect } from 'chai'
+
+describe('Logging in @watch', () => {
+  it('Can login a registered user', () => {
+    browser.url('http://localhost:3000/')
+    browser.setValue('#username', 'Morgan.Buckridge12')
+    browser.setValue('#password', 'cpucoe')
+    browser.click('#submitButton')
+    const doesExist = browser.waitForExist('#navBar')
+    expect(doesExist).to.equal(true)
+  })
+  it('Cannot login an unregistered user', () => {
+    browser.url('http://localhost:3000/')
+    browser.setValue('#username', 'Morgan')
+    browser.setValue('#password', 'cpucoe')
+    browser.click('#submitButton')
+    browser.waitForExist('#toast')
+    const message = browser.getText('#toast')
+    expect(message).to.contain('User not found')
+  })
+})
