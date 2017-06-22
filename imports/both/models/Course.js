56a37d9ce4e701016e90f667a0ed72f8ad3e686e
Sian Paul Lasaga <sianpaullasaga@gmail.com>
2017-06-22 22:23:07 +0800

    Added methods for scores and standing calculations

--- imports/both/models/Course.js
+++ imports/both/models/Course.js
@@ -21,12 +21,15 @@ class Course extends Model {
 
   enrollAStudent(student) {
     const studentIds = this.studentIds
-    const isStudentExists = studentIds.some(studentId => student._id === studentId)
-    if (!isStudentExists) {
+    if (!this.isStudentEnrolled(student)) {
       studentIds.push(student._id)
     } else {
       throw new Error('Student is already enrolled.')
     }
+  }
+
+  isStudentEnrolled(student) {
+    return this.studentIds.some(studentId => student._id === studentId)
   }
 
   @Idempotent
@@ -159,6 +162,68 @@ class Course extends Model {
       student.records = records.map(record => _(record).omit('studentId'))
       return student
     })
+  }
+
+  get classRecord() {
+    this.students.map((student) => {
+      const computeRecords = this.computeStudentRecords(student)
+      const standing = this.fullGradingTemplate.computeStanding(computeRecords)
+      console.log(computeRecords)
+      console.log(standing)
+    })
+  }
+
+  computeStudentRecords(student) {
+    const records = this.activityRecords.filter(record => record.studentId === student._id)
+    const computedRecords = this.computeRecords(records)
+    return computedRecords
+  }
+
+  computeActivityPercentages(scoresDoc) {
+    const doc = scoresDoc
+    this.activityTypesWithScores.forEach((type) => {
+      const percentage = ((doc[type.name].score / type.totalScore) * 100).toFixed(4)
+      Object.assign(doc[type.name], { percentage: percentage })
+    })
+    return doc
+  }
+
+  computeRecords(records) {
+    const scoresDoc = records.reduce((acc, cur, index, arr) => {
+      const activityType = cur.activityType
+      if (index === 1) {
+        const obj = {}
+        const arr1 = arr[0]
+        const arr2 = arr[1]
+        if (arr1.activityType === arr2.activityType) {
+          obj[cur.activityType] = { score: arr1.score + arr2.score }
+        } else {
+          obj[arr1.activityType] = { score: arr1.score }
+          obj[arr2.activityType] = { score: arr2.score }
+        }
+        return obj
+      } else if (index > 1 && !acc[activityType]) {
+        acc[activityType] = { score: cur.score }
+      } else {
+        acc[activityType].score += cur.score
+      }
+      return acc
+    })
+    this.activityTypesWithScores.forEach((type) => {
+      const percentage = ((scoresDoc[type.name].score / type.totalScore) * 100).toFixed(4)
+      Object.assign(scoresDoc[type.name], { percentage: percentage })
+    })
+    return scoresDoc
+  }
+
+  getStudentStanding(student) {
+    const records = this.activityRecords.filter(record => record.studentId === student._id)
+
+  }
+
+  getStudentGradeSummary(student) {
+    const activityRecords = this.activityRecords
+    const records = activityRecords.filter(record => record.studentId === student._id)
   }
 }
 
--- imports/both/models/GradingTemplate.js
+++ imports/both/models/GradingTemplate.js
@@ -42,6 +42,28 @@ class GradingTemplate extends Model {
     }
   }
 
+  computeStanding(scoresDoc) {
+    const categories = this.categories
+    const resultArray = categories.map((category) => {
+      const doc = {}
+      const types = this.getActivityTypes(category)
+      const activitiesPercentages = types.map(type => (scoresDoc[type.name].percentage / 100) * type.percentage)
+      doc[category] = activitiesPercentages.reduce((acc, cur) => acc + cur)
+      return doc
+    })
+    if (categories.length > 1) {
+      return Object.assign(resultArray[0], resultArray[1])
+    }
+    return resultArray[0]
+  }
+
+  computeTotalScores(activities) {
+    const categories = this.categories
+    categories.map((category) => {
+      const activityTypes = this.activityTypes
+    })
+  }
+
   get categories() {
     return (this.laboratory) ? ['lecture', 'laboratory'] : ['lecture']
   }
