39f7c09d0086fc53b0485f808e30f1e4d2989881
Sian Paul Lasaga <sianpaullasaga@gmail.com>
2017-07-05 23:44:39 +0800

    Added Student Courses Component(for student courses sched, grades, etc viewing)

---
+++ imports/client/controllers/StudentCoursesComponent.js
@@ -0,0 +1,48 @@
+import Student from '/imports/both/models/Student'
+import { Meteor } from 'meteor/meteor'
+import { Component, State, Inject } from 'angular2-now'
+import '../views/student-courses.html'
+
+@State({
+  name: 'app.student.courses',
+  url: '/student/courses/:studentId',
+ /* resolve: {
+    redirect($auth, $location) {
+      $auth.awaitUser().then((user) => {
+        if (user.hasARole('faculty')) {
+          $location.path('/login')
+        }
+      })
+    },
+  },*/
+})
+@Component({
+  selector: 'student-courses',
+  templateUrl: 'imports/client/views/student-courses.html',
+})
+@Inject('$scope', '$reactive', '$state', '$stateParams')
+class StudentCoursesComponent {
+
+  constructor($scope, $reactive, $state, $stateParams) {
+    $reactive(this).attach($scope)
+    const { studentId } = $stateParams
+    this.subscribe('students', () => {
+      const courseSubs = this.subscribe('courses')
+      const sessionSubs = this.subscribe('sessions')
+      const activitySubs = this.subscribe('activities')
+      const activityTypeSubs = this.subscribe('activity-types')
+      const gradingSubs = this.subscribe('grading-templates')
+      const settingSubs = this.subscribe('settings')
+      const gradeTransmutationSubs = this.subscribe('grade-transmutations')
+      this.student = Student.findOne({ _id: studentId })
+      const student = this.student
+      if (courseSubs.ready() && sessionSubs.ready() && activitySubs.ready()
+          && activitySubs.ready() && activityTypeSubs.ready() && gradingSubs.ready()
+          && settingSubs.ready() && gradeTransmutationSubs.ready() && student) {
+        this.records = student.coursesGrades
+      }
+    })
+  }
+}
+
+export default StudentCoursesComponent
