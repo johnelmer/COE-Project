0f73ed5ec193b81005ccd475e50e3dffd003c77a
James <jamesb9617@gmail.com>
2017-07-11 21:14:34 +0800

    fixed notification component

--- imports/client/controllers/NotificationComponent.js
+++ imports/client/controllers/NotificationComponent.js
@@ -21,17 +21,19 @@ import '../views/notification.html'
   selector: 'notification-view',
   templateUrl: 'imports/client/views/notification.html',
 })
-@Inject('scope', '$reactive', '$stateParams')
+@Inject('$scope', '$reactive', '$stateParams', '$state')
 class NotificationComponent {
   // static schema = schema
-  constructor($scope, $reactive, $stateParams) {
+  constructor($scope, $reactive, $stateParams, $state) {
     $reactive(this).attach($scope)
     this.subscribe('notifications')
     this.helpers({
       notifications() {
-        if ($state.current.name.endsWith('show')) {
-          return Meteor.user().notifications
+        const user = Meteor.user()
+        if (user) {
+          return user.notifications
         }
+        return []
       }
     })
   }
