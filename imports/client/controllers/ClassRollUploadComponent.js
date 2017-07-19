8a4a123f2671d9ceef69f8113b3a73ca61a8fa22
Vince Paul Dela Cruz <sekiryuutei47@gmail.com>
2017-07-19 12:41:41 +0800

    feat(Class Roll): Upload + Database Save

--- imports/client/controllers/ClassRollUploadComponent.js
+++ imports/client/controllers/ClassRollUploadComponent.js
@@ -1,6 +1,10 @@
 /* eslint no-alert: "off" */
 /* eslint-disable no-param-reassign */
 import { Component, State, Inject } from 'angular2-now'
+import { Meteor } from 'meteor/meteor'
+import { Tracker } from 'meteor/tracker'
+import Course from '/imports/both/models/Course'
+import Student from '/imports/both/models/Student'
 import XLSX from 'xlsx'
 import 'ng-file-upload/dist/ng-file-upload.min.js'
 import '../views/classroll-upload.html'
@@ -8,6 +12,21 @@ import '../views/classroll-upload.html'
 @State({
   name: 'app.classroll.upload',
   url: '/classroll/upload',
+  resolve: {
+    subs() {
+      return new Promise((resolve) => {
+        Tracker.autorun(() => {
+          const students = Meteor.subscribe('students-basic-infos')
+          const courses = Meteor.subscribe('courses')
+          const subs = [courses, students]
+          const isReady = subs.every(sub => sub.ready())
+          if (isReady) {
+            resolve(true)
+          }
+        })
+      })
+    },
+  },
 })
 @Component({
   selector: 'classroll-upload',
@@ -54,7 +73,29 @@ class ClassRollUploadComponent {
           const firstSheetName = workbook.SheetNames[0]
           const workSheet = workbook.Sheets[firstSheetName]
           const json = XLSX.utils.sheet_to_json(workSheet, { range: 7, raw: true });
-          const stubcode = workSheet['R3']['v']
+          const stubcode = parseInt(workSheet.R3.v, 10)
+          const studentIdNumbers = json.map(student => student['ID. No.'])
+          this.course = Course.findOne({ stubcode: stubcode })
+          this.students = Student.find({ idNumber: { $in: studentIdNumbers } }).fetch()
+          console.log(this.course);
+          console.log(this.students);
+        })
+      })
+    }
+  }
+
+  save() {
+    if (this.file) {
+      new Promise((resolve) => {
+        resolve(this.students.forEach((student) => {
+          this.course.enrollAStudent(student)
+        }))
+      })
+      .then(() => {
+        this.course.save(() => {
+          const { length } = this.students
+          const { stubcode } = this.course
+          alert(`Enrolled ${length} to stubcode ${stubcode}`)
         })
       })
     }
