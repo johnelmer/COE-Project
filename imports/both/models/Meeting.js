26f8fd623cf816dff403696d66539f8ee37640f0
James <jamesb9617@gmail.com>
2017-07-20 01:31:06 +0800

    modified code

--- imports/both/models/Meeting.js
+++ imports/both/models/Meeting.js
@@ -2,6 +2,7 @@ import SetupCollection from '../decorators/SetupCollection'
 import Notification from './Notification'
 import schema from '../schemas/Meeting.js'
 import Model from './Model'
+import Idempotent from '../decorators/Idempotent'
 
 @SetupCollection('Meetings')
 class Meeting extends Model {
@@ -23,7 +24,16 @@ class Meeting extends Model {
         this.notificationId = doc
       }
     })
-  }
+    }
+    
+    @Idempotent
+    get attendees() {
+      return User.find({ _id: { $in: this.attendeeIds }}).fetch()
+    }
+
+    get notification() {
+      return Notification.find({ _id: { $in: this.notificationId }})
+    }
 }
 
 export default Meeting
