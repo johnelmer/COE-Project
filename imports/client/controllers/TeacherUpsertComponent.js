06da172e7851ccf1dccb7f7d45fc29ddfb018627
Vince Paul Dela Cruz <sekiryuutei47@gmail.com>
2017-07-21 04:19:08 +0800

    fix(Unnecessary Publish): Subject Upsert

--- imports/client/controllers/TeacherUpsertComponent.js
+++ imports/client/controllers/TeacherUpsertComponent.js
@@ -1,6 +1,7 @@
 import User from '/imports/both/models/User'
 import schema from '/imports/both/schemas/User'
 import { Meteor } from 'meteor/meteor'
+import { Tracker } from 'meteor/tracker'
 import _ from 'underscore'
 import Department from '/imports/both/models/Department'
 import { Component, State, Inject } from 'angular2-now'
@@ -13,6 +14,19 @@ import '../views/teacher-upsert.html'
     redirect($location) {
       const isAuthorized = Meteor.user().hasARole('secretary')
       return isAuthorized || $location.path('/login')
+    },
+    subs() {
+      return new Promise((resolve) => {
+        Tracker.autorun(() => {
+          const departments = Meteor.subscribe('departments')
+          const teachers = Meteor.subscribe('teachers')
+          const subs = [departments, teachers]
+          const isReady = subs.every(sub => sub.ready())
+          if (isReady) {
+            resolve(true)
+          }
+        })
+      })
     },
   },
 })
@@ -30,8 +44,6 @@ class TeacherUpsertComponent {
   constructor($scope, $reactive, $state, $stateParams, ngToast) {
     $reactive(this).attach($scope)
     this.buttonLabel = ''
-    this.subscribe('departments')
-    this.subscribe('teachers')
     const { teacherId } = $stateParams
     if ($state.current.name.endsWith('create')) {
       this.buttonLabel = 'Register'
