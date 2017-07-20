2f67ca786e500ab49f65afb2132572bcf0ed425b
Vince Paul Dela Cruz <sekiryuutei47@gmail.com>
2017-07-21 05:53:20 +0800

    perf(Pub/Sub in Change Password Component):

--- imports/client/controllers/ChangePasswordComponent.js
+++ imports/client/controllers/ChangePasswordComponent.js
@@ -1,11 +1,27 @@
 import { Component, State, Inject } from 'angular2-now'
 import AppSetting from '/imports/both/models/AppSetting'
 import { Meteor } from 'meteor/meteor'
+import { Tracker } from 'meteor/tracker'
 import '../views/change-password.html'
 
 @State({
   name: 'app.settings.changePassword',
-  url: '/settings/changepassword',	
+  url: '/settings/changepassword',
+  resolve: {
+    subs() {
+      return new Promise((resolve) => {
+        Tracker.autorun(() => {
+          const settings = this.subscribe('settings')
+          const currentUser = this.subscribe('currentUser')
+          const subs = [currentUser, settings]
+          const isReady = subs.every(sub => sub.ready())
+          if (isReady) {
+            resolve(true)
+          }
+        })
+      })
+    }
+  }
 })
 @Component({
   selector: 'settings-change',
@@ -13,10 +29,8 @@ import '../views/change-password.html'
 })
 @Inject('$scope', '$reactive', '$state', '$stateParams')
 class ChangePasswordComponent {
-  constructor($scope, $reactive, $state, $stateParams) {
+  constructor($scope, $reactive) {
     $reactive(this).attach($scope)
-    this.subscribe('settings')
-    this.subscribe("currentUser")
     this.user = Meteor.user()
   }
 
