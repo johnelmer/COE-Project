1525640dbdc84bf984a6abba85524950f3ea8709
Vince Paul Dela Cruz <sekiryuutei47@gmail.com>
2017-07-21 02:22:14 +0800

    feat(Update instructor for courses):

--- imports/client/controllers/CourseUpdateComponent.js
+++ imports/client/controllers/CourseUpdateComponent.js
@@ -2,6 +2,7 @@ import Course from '/imports/both/models/Course'
 import Role from '/imports/both/models/Role'
 import User from '/imports/both/models/User'
 import { Meteor } from 'meteor/meteor'
+import { Tracker } from 'meteor/tracker'
 import { Component, State, Inject } from 'angular2-now'
 import '../views/course-update.html'
 
@@ -12,6 +13,19 @@ import '../views/course-update.html'
     redirect($location) {
       const isAuthorized = Meteor.user().hasARole('faculty')
       return isAuthorized || $location.path('/login')
+    },
+    resolve() {
+      return new Promise((resolve) => {
+        Tracker.autorun(() => {
+          const courses = Meteor.subscribe('courses')
+          const teachers = Meteor.subscribe('teachers')
+          const subs = [teachers, courses]
+          const subsReady = subs.every(sub => sub.ready())
+          if (subsReady) {
+            resolve(true)
+          }
+        })
+      })
     },
   },
 })
@@ -24,28 +38,37 @@ class CourseUpdateComponent {
   constructor($scope, $reactive, $stateParams) {
     $reactive(this).attach($scope)
     const { courseId } = $stateParams
-    this.subscribe('courses')
-    this.subscribe('users')
     this.helpers({
       course() {
         return Course.findOne({ _id: courseId })
       },
       teachers() {
-        return User.find({ roleName: 'faculty' }).fetch()
-      }
+        return User.find().fetch()
+      },
     })
   }
 
   save() {
-    if (this.teacher !== undefined ) {
-      const labTime = this.course.laboratory.time
-      const labRoom = this.course.laboratory.room
-      const labTeacher = this.teacher.fullName
-      const labTeacherId = this.teacher.idNumber
-      const labUserId = this.teacher._id
-      this.course.laboratory = { instructor:{ _id: labUserId, fullName: labTeacher, idNumber: labTeacherId }, time: labTime, room: labRoom}
+    if (this.course.laboratory.instructor) {
+      const { instructor } = this.course.laboratory
+      const { _id, fullName, idNumber } = instructor
+      const lab = {
+        instructor: {
+          _id: _id,
+          fullName: fullName,
+          idNumber: idNumber,
+        },
+      }
+      Object.assign(this.course.laboratory, lab)
     }
-    this.course.save(err => {
+    const { instructor } = this.course.lecture
+    const { _id, fullName, idNumber } = instructor
+    this.course.lecture.instructor = {
+      _id: _id,
+      fullName: fullName,
+      idNumber: idNumber,
+    }
+    this.course.save((err) => {
       if (err) {
         console.log(err);
       }
