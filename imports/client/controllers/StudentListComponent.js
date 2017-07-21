c2ff5a3742b52ecd7bf1eb30ae442b3517481a87
Vince Paul Dela Cruz <sekiryuutei47@gmail.com>
2017-07-21 22:20:02 +0800

    feat(Excel Download For Printing Student Lists):

--- imports/client/controllers/StudentListComponent.js
+++ imports/client/controllers/StudentListComponent.js
@@ -2,6 +2,8 @@ import Student from '/imports/both/models/Student'
 import { Meteor } from 'meteor/meteor'
 import { Tracker } from 'meteor/tracker'
 import { Component, State, Inject } from 'angular2-now'
+import angular from 'angular'
+import XLSX from 'xlsx'
 import '../views/student-list.html'
 
 @State({
@@ -30,19 +32,57 @@ import '../views/student-list.html'
   selector: 'student-list',
   templateUrl: 'imports/client/views/student-list.html',
 })
-@Inject('$scope', '$reactive', '$uibModal')
+@Inject('$scope', '$reactive', '$location', '$http')
 class StudentListComponent {
 
-  constructor($scope, $reactive) {
+  constructor($scope, $reactive, $location, $http) {
     $reactive(this).attach($scope)
     this.helpers({
       students() {
         return Student.find().fetch()
       },
     })
+    this.$http = $http
+    this.$location = $location
     this.min = 10
     this.limit = this.limit || this.min
     this.max = this.students.length
+    this.url = this.url || window.location.href
+    this.fileName = this.fileName || 'Download.xlsx'
+    // setInterval(() => {
+    //   console.log(this.filteredStudents);
+    // }, 1000)
+  }
+
+  print() {
+    const json = this.filteredStudents.map((entry) => {
+      const properFormat = angular.copy(entry)
+      const student = new Student(properFormat)
+      return student.doc
+    })
+    const sheet = XLSX.utils.json_to_sheet(json)
+    //
+    // const html = XLSX.utils.sheet_to_html(sheet)
+    // this.content = html
+    //
+    const workbook = {
+      SheetNames: [],
+      Sheets: {
+      },
+    }
+    const sheetName = 'Students List'
+    workbook.SheetNames.push(sheetName)
+    workbook.Sheets[sheetName] = sheet
+    const wopts = {
+      bookType: 'xlsx',
+      bookSST: false,
+      type: 'base64',
+    }
+    const base64URL = XLSX.write(workbook, wopts);
+    const dataType = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64'
+    const url = `${dataType},${base64URL}`
+    this.fileName = `${sheetName}.${wopts.bookType}`
+    this.url = url
   }
 
   get isStudentsReady() {
